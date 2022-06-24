79af431fcd43fa3ae724a9824e79e9307e8fe116
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-06-24 15:46:09 +0200

    up
    Modification des fichiers pour le thème enfant, le fichier style est terminé

--- functions.php
+++ functions.php
@@ -7,12 +7,3 @@ function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-
-function wpb_hook_javascript_footer() {
-    ?>
-    <script>
-       
-    </script>
-        <?php
-      }
-    add_action('wp_footer', 'wpb_hook_javascript_footer');
